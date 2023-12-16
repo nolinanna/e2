b1966e882e65b0f94804081a3edaa91e15fb33ab
Anna Nolin <annapricots@gmail.com>
2023-12-16 23:15:35 +0000

    Project 3 migrations & seeds

--- p3/app/Commands/AppCommand.php
+++ p3/app/Commands/AppCommand.php
@@ -2,10 +2,38 @@
 
 namespace App\Commands;
 
+use Faker\Factory;
+
 class AppCommand extends Command
 {
-    public function test()
+    public function fresh(){
+        $this->migrate();
+        $this->seed();
+    }
+    
+    public function migrate()
     {
-        dump('It works! You invoked your first command.');
+        $this->app->db()->createTable('rounds', [
+            'choice' => 'varchar(8)',
+            'result' => 'varchar(4)',
+            'timestamp' => 'timestamp',
+        ]);
+
+        dump('Migration Complete');
+    }
+
+    public function seed()
+    {
+        $faker = Factory::create();
+        
+        for($i = 10; $i > 0; $i--) {
+            $this->app->db()->insert('rounds', [
+                'choice' => ['rock', 'paper', 'scissors'][rand(0,2)],
+                'result' => ['win', 'lose', 'tie'][rand(0,2)],
+                'timestamp' => $faker->dateTimeBetween('-'.$i.' days', '-'.$i.' days')->format('Y-m-d H:i:s')
+            ]);
+        }
+
+        dump('Seeds Complete');
     }
 }
